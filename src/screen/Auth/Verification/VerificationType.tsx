f159fe24cd5b2a602d8edc3a3f6b14473d385640
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2023-12-18 22:50:44 -0800

    new push for verification

--- src/screen/Auth/Verification/VerificationType.tsx
+++ src/screen/Auth/Verification/VerificationType.tsx
@@ -11,10 +11,49 @@ import {COLORS, SIZES, FONTS, icons} from '../../../constants';
 import HeaderA from '../../../components/Header/HeaderA';
 
 const VerificationType = () => {
+  const verificationData = [
+    {
+      id: 1,
+      title: 'International Passport',
+    },
+    {
+      id: 2,
+      title: 'Driving Licence',
+    },
+    {
+      id: 3,
+      title: 'Student ID',
+    },
+    {
+      id: 4,
+      title: 'NIN',
+    },
+  ];
   return (
     <View style={styles.page}>
       <HeaderA />
-      <Text>VerificationType</Text>
+      <Text style={{...FONTS.h1, color: COLORS.black}}>Photo of your ID</Text>
+      <Text style={{...FONTS.body3, color: COLORS.black}}>
+        Choose which type of documents you're going to use.
+      </Text>
+      <View>
+        <FlatList
+          data={verificationData}
+          renderItem={({item}) => {
+            return (
+              <View style={styles.container}>
+                <Text style={{...FONTS.body3, color: COLORS.black}}>
+                  {item.title}
+                </Text>
+                <Image
+                  source={icons.arrowright}
+                  style={{height: SIZES.h3, width: SIZES.h3}}
+                />
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -25,7 +64,15 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: COLORS.white,
-    paddingHorizontal: SIZES.width * 0.05,
+    paddingHorizontal: SIZES.width * 0.04,
     paddingTop: SIZES.h5,
+  },
+  container: {
+    height: SIZES.h1,
+    borderWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: SIZES.base,
   },
 });
