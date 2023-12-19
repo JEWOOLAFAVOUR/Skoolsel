c4eee8971c1e390682ee7ca3894cc85948ea1bf7
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2023-12-18 22:15:47 -0800

    monday push

--- src/constants/icons.tsx
+++ src/constants/icons.tsx
@@ -1,19 +1,25 @@
 export const arrowleft = require('../../assets/icons/arrowleft.png');
+export const arrowright = require('../../assets/icons/arrowright.png');
 export const avatar = require('../../assets/icons/avatar.png');
 export const badge = require('../../assets/icons/badge.png');
 export const bookmark = require('../../assets/icons/bookmark.png');
 export const close = require('../../assets/icons/close.png');
 export const empty = require('../../assets/icons/empty.png');
+export const lock = require('../../assets/icons/lock.png');
+export const mark = require('../../assets/icons/mark.png');
 export const product = require('../../assets/icons/product.png');
 export const verticalmenu = require('../../assets/icons/verticalmenu.png');
 
 export default {
   arrowleft,
+  arrowright,
   avatar,
   badge,
   bookmark,
   close,
   empty,
+  lock,
+  mark,
   product,
   verticalmenu,
 };
--- src/constants/images.tsx
+++ src/constants/images.tsx
@@ -1,9 +1,11 @@
 export const image1 = require('../../assets/images/image1.png');
 export const image2 = require('../../assets/images/image2.png');
+export const image3 = require('../../assets/images/image3.png');
 export const logo = require('../../assets/images/logo.png');
 
 export default {
   image1,
   image2,
+  image3,
   logo,
 };
