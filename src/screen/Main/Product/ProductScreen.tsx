08135bdff9937a92d4ced5480d166e8fcd72f680
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2023-12-22 05:58:21 -0800

    product top bar fixed

--- src/screen/Main/Product/ProductScreen.tsx
+++ src/screen/Main/Product/ProductScreen.tsx
@@ -72,150 +72,190 @@ const ProductScreen = () => {
 
   return (
     <View style={styles.page}>
-      <View>
-        <FlatList
-          // data={productData}
-          data={product}
-          showsVerticalScrollIndicator={false}
-          ListEmptyComponent={RenderEmpty}
-          renderItem={({item}) => {
-            return (
-              <TouchableOpacity
-                onPress={() =>
-                  // navigation.navigate('ProductDetails', {data: item})
-                  navigation.navigate('SearchScreen', {data: item})
-                }
-                SearchScreen
-                style={{marginBottom: SIZES.h2}}>
-                <View style={{paddingHorizontal: SIZES.width * 0.03}}>
-                  <View
-                    style={{
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                      marginBottom: SIZES.h5,
-                    }}>
-                    <Image
-                      source={item.seller.avatar}
-                      style={{
-                        height: SIZES.h1 * 1.2,
-                        width: SIZES.h1 * 1.2,
-                        borderRadius: 100,
-                      }}
-                    />
-                    <View style={{marginLeft: SIZES.base, flex: 1}}>
-                      <View
-                        style={{flexDirection: 'row', alignItems: 'center'}}>
-                        <Text
-                          style={{
-                            fontSize: SIZES.h4,
-                            color: COLORS.black,
-                            fontFamily: 'OpenSans-Bold',
-                          }}>
-                          @{item.seller.username}
-                        </Text>
-                        <Image
-                          source={item.seller.verified === true && icons.badge}
-                          style={{
-                            height: SIZES.h3,
-                            width: SIZES.h3,
-                            marginLeft: SIZES.base * 0.5,
-                          }}
-                        />
-                      </View>
-                      <Text
-                        style={{
-                          fontSize: SIZES.body5 * 0.9,
-                          color: COLORS.black,
-                          fontFamily: 'OpenSans-Regular',
-                        }}>
-                        {item.seller.location}
-                      </Text>
-                    </View>
-                    <TouchableOpacity onPress={() => openBottomSheet()}>
-                      <Image
-                        source={icons.verticalmenu}
-                        style={{height: SIZES.h3, width: SIZES.h3}}
-                      />
-                    </TouchableOpacity>
-                  </View>
-                  <View>
-                    {/* SLIDER */}
-                    <View>
-                      <ImageSliderComponent imgData={item.productImage} />
-                      <View style={styles.priceCtn}>
-                        <Text style={{...FONTS.body4, color: COLORS.white}}>
-                          ₦{item.price}
-                        </Text>
-                      </View>
-                    </View>
-                    {/* CATEGORY  */}
+      <View style={{marginBottom: SIZES.h1 * 2.5}}>
+        {/* HEADER */}
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            paddingHorizontal: SIZES.width * 0.03,
+          }}>
+          <Text
+            style={{
+              fontSize: SIZES.h1,
+              color: COLORS.primary,
+              fontFamily: 'OpenSans-Bold',
+            }}>
+            Skool<Text style={{color: COLORS.black}}>sel</Text>
+          </Text>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <TouchableOpacity
+              style={{marginRight: SIZES.h3}}
+              onPress={() => navigation.navigate('SearchScreen')}>
+              <Image
+                source={icons.search}
+                style={{height: SIZES.h1, width: SIZES.h1}}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image
+                source={icons.avatar}
+                style={{
+                  height: SIZES.h1,
+                  width: SIZES.h1,
+                  borderRadius: 100,
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={{marginTop: SIZES.h5}}>
+          <FlatList
+            // data={productData}
+            data={product}
+            showsVerticalScrollIndicator={false}
+            ListEmptyComponent={RenderEmpty}
+            renderItem={({item}) => {
+              return (
+                <TouchableOpacity
+                  onPress={() =>
+                    navigation.navigate('ProductDetails', {data: item})
+                  }
+                  SearchScreen
+                  style={{marginBottom: SIZES.h2}}>
+                  <View style={{paddingHorizontal: SIZES.width * 0.03}}>
                     <View
                       style={{
                         flexDirection: 'row',
                         alignItems: 'center',
-                        justifyContent: 'space-between',
-                        marginTop: SIZES.h5,
-                        marginBottom: SIZES.base,
+                        marginBottom: SIZES.h5,
                       }}>
+                      <Image
+                        source={item.seller.avatar}
+                        style={{
+                          height: SIZES.h1 * 1.2,
+                          width: SIZES.h1 * 1.2,
+                          borderRadius: 100,
+                        }}
+                      />
+                      <View style={{marginLeft: SIZES.base, flex: 1}}>
+                        <View
+                          style={{flexDirection: 'row', alignItems: 'center'}}>
+                          <Text
+                            style={{
+                              fontSize: SIZES.h4,
+                              color: COLORS.black,
+                              fontFamily: 'OpenSans-Bold',
+                            }}>
+                            @{item.seller.username}
+                          </Text>
+                          <Image
+                            source={
+                              item.seller.verified === true && icons.badge
+                            }
+                            style={{
+                              height: SIZES.h3,
+                              width: SIZES.h3,
+                              marginLeft: SIZES.base * 0.5,
+                            }}
+                          />
+                        </View>
+                        <Text
+                          style={{
+                            fontSize: SIZES.body5 * 0.9,
+                            color: COLORS.black,
+                            fontFamily: 'OpenSans-Regular',
+                          }}>
+                          {item.seller.location}
+                        </Text>
+                      </View>
+                      <TouchableOpacity onPress={() => openBottomSheet()}>
+                        <Image
+                          source={icons.verticalmenu}
+                          style={{height: SIZES.h3, width: SIZES.h3}}
+                        />
+                      </TouchableOpacity>
+                    </View>
+                    <View>
+                      {/* SLIDER */}
+                      <View>
+                        <ImageSliderComponent imgData={item.productImage} />
+                        <View style={styles.priceCtn}>
+                          <Text style={{...FONTS.body4, color: COLORS.white}}>
+                            ₦{item.price}
+                          </Text>
+                        </View>
+                      </View>
+                      {/* CATEGORY  */}
+                      <View
+                        style={{
+                          flexDirection: 'row',
+                          alignItems: 'center',
+                          justifyContent: 'space-between',
+                          marginTop: SIZES.h5,
+                          marginBottom: SIZES.base,
+                        }}>
+                        <Text
+                          style={{
+                            ...FONTS.body4,
+                            color: COLORS.black,
+                            fontFamily: 'OpenSans-Medium',
+                          }}>
+                          {item.category}
+                        </Text>
+                        <TouchableOpacity>
+                          <Image
+                            source={icons.bookmark}
+                            style={{
+                              height: SIZES.h2 * 1.1,
+                              width: SIZES.h2 * 1.1,
+                            }}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                      {/* DETAILS */}
                       <Text
                         style={{
                           ...FONTS.body4,
                           color: COLORS.black,
                           fontFamily: 'OpenSans-Medium',
                         }}>
-                        {item.category}
+                        {item.title}
                       </Text>
-                      <TouchableOpacity>
-                        <Image
-                          source={icons.bookmark}
-                          style={{
-                            height: SIZES.h2 * 1.1,
-                            width: SIZES.h2 * 1.1,
-                          }}
-                        />
-                      </TouchableOpacity>
+                      <Text style={{...FONTS.body4b, color: COLORS.black}}>
+                        Introducing our sleek and versatile crossbody bag,
+                        perfect for any occasion. With multiple compactment
+                      </Text>
+                      <Text style={{...FONTS.body5a, color: COLORS.black}}>
+                        {item.createdAt}
+                      </Text>
                     </View>
-                    {/* DETAILS */}
-                    <Text
-                      style={{
-                        ...FONTS.body4,
-                        color: COLORS.black,
-                        fontFamily: 'OpenSans-Medium',
-                      }}>
-                      {item.title}
-                    </Text>
-                    <Text style={{...FONTS.body4b, color: COLORS.black}}>
-                      Introducing our sleek and versatile crossbody bag, perfect
-                      for any occasion. With multiple compactment
-                    </Text>
-                    <Text style={{...FONTS.body5a, color: COLORS.black}}>
-                      {item.createdAt}
-                    </Text>
                   </View>
-                </View>
-                <View
-                  style={{
-                    height: 1,
-                    backgroundColor: COLORS.chocolateBackground,
-                    marginTop: SIZES.h4,
-                  }}
-                />
-              </TouchableOpacity>
-            );
-          }}
-        />
-        {/* ADD ICON  */}
-        {product.length > 0 && (
-          <TouchableOpacity
-            activeOpacity={0.7}
-            onPress={() => navigation.navigate('UploadScreen')}
-            style={{position: 'absolute', bottom: SIZES.h3, right: SIZES.h4}}>
-            <Image
-              source={icons.bigadd}
-              style={{height: SIZES.h1 * 2, width: SIZES.h1 * 2}}
-            />
-          </TouchableOpacity>
-        )}
+                  <View
+                    style={{
+                      height: 1,
+                      backgroundColor: COLORS.chocolateBackground,
+                      marginTop: SIZES.h4,
+                    }}
+                  />
+                </TouchableOpacity>
+              );
+            }}
+          />
+          {/* ADD ICON  */}
+          {product.length > 0 && (
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => navigation.navigate('UploadScreen')}
+              style={{position: 'absolute', bottom: SIZES.h3, right: SIZES.h4}}>
+              <Image
+                source={icons.bigadd}
+                style={{height: SIZES.h1 * 2, width: SIZES.h1 * 2}}
+              />
+            </TouchableOpacity>
+          )}
+        </View>
       </View>
       {/* BOTTOM SHEET  */}
       <RBSheet
@@ -326,7 +366,7 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: COLORS.white,
-    paddingTop: SIZES.h5,
+    // paddingTop: SIZES.h5,
   },
   bottomText: {
     color: COLORS.black,
